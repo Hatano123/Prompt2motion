2690902457f1cab94c94bd79bed7bb0a96cf4541
HATANO Yuto <y263357@ydev08.edu.tut.ac.jp>
2026-09-14 19:22:52 +0900

    Pass multi-camera PBS variables safely

--- scripts/submit_paired_pilot.sh
+++ scripts/submit_paired_pilot.sh
@@ -14,11 +14,12 @@ fi
 for spec in 'single_top:top' 'single_side:side' 'multi:top,side'; do
   model_name="${spec%%:*}"
   camera_names="${spec#*:}"
+  export MODEL_NAME="$model_name" CAMERA_NAMES="$camera_names"
   if [[ -n "$dependency" ]]; then
-    train_job="$(qsub -W depend="$dependency" -v MODEL_NAME="$model_name",CAMERA_NAMES="$camera_names" scripts/run_paired_pilot_train.pbs)"
+    train_job="$(qsub -W depend="$dependency" -v MODEL_NAME,CAMERA_NAMES scripts/run_paired_pilot_train.pbs)"
   else
-    train_job="$(qsub -v MODEL_NAME="$model_name",CAMERA_NAMES="$camera_names" scripts/run_paired_pilot_train.pbs)"
+    train_job="$(qsub -v MODEL_NAME,CAMERA_NAMES scripts/run_paired_pilot_train.pbs)"
   fi
-  eval_job="$(qsub -W depend="afterok:$train_job" -v MODEL_NAME="$model_name",CAMERA_NAMES="$camera_names" scripts/run_paired_pilot_eval.pbs)"
+  eval_job="$(qsub -W depend="afterok:$train_job" -v MODEL_NAME,CAMERA_NAMES scripts/run_paired_pilot_eval.pbs)"
   echo "$model_name train=$train_job eval=$eval_job"
 done
